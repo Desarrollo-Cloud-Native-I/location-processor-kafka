5df9313178ec2f94cca207a733e3081be574855c
BriamCarrasco <briamcarrasco.f@gmail.com>
2026-02-24 22:36:12 -0300

    Archivos iniciales

---
+++ src/main/java/com/dcni/ubication_processor/UbicationProcessorApplication.java
@@ -0,0 +1,14 @@
+package com.dcni.ubication_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbicationProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbicationProcessorApplication.class, args);
+    }
+
+}
+ 
---
+++ src/test/java/com/dcni/ubication_processor/UbicationProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcni.ubication_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbicationProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
